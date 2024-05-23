81111be12b80c4d33c15ed4b8d368b705bfee25a
Nahuel Cassinari <matandomuertos@users.noreply.github.com>
2024-05-23 16:30:56 +0200

    deleted test print

--- jjkiller/queue_utils.py
+++ jjkiller/queue_utils.py
@@ -3,7 +3,6 @@ from .common_utils import get_running_time, timeout
 
 def queued_builds(server, killtime, queue_list):
     queued_builds = server.get_queue_info()
-    print(queued_builds)
     if not queued_builds:
         print("The queue is empty")
         return
